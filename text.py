a4d39aebc4e6d87ac44deeae1f63d0be545c114b
Shamsiddin <135255247+Muxitdinovich@users.noreply.github.com>
2023-06-03 13:04:42 +0500

    Update text.py

--- text.py
+++ text.py
@@ -61,10 +61,10 @@ class TextGroup(object):
         self.alltext[SCORETXT] = Text("0".zfill(8), WHITE, 0, TILEHEIGHT, size)
         self.alltext[LEVELTXT] = Text(str(1).zfill(3), WHITE, 23*TILEWIDTH, TILEHEIGHT, size)
         self.alltext[READYTXT] = Text("Hello!", YELLOW, 11.25*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
-        self.alltext[PAUSETXT] = Text("STOP!", YELLOW, 10.625*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
-        self.alltext[GAMEOVERTXT] = Text("Yutqazdingizh!", YELLOW, 10*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
-        self.addText("Hisob", WHITE, 0, 0, size)
-        self.addText("Bosqich", WHITE, 23*TILEWIDTH, 0, size)
+        self.alltext[PAUSETXT] = Text("PAUSE!", YELLOW, 10.625*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
+        self.alltext[GAMEOVERTXT] = Text("GAME OVER!", YELLOW, 10*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
+        self.addText("Счет", WHITE, 0, 0, size)
+        self.addText("Уревень", WHITE, 23*TILEWIDTH, 0, size)
 
     def update(self, dt):
         for tkey in list(self.alltext.keys()):
